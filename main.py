c09b49e38dabbdb70f8d3b0a602b0e42d7533cd9
nycolaskaike <nycolasoliveira2604@gmail.com>
2026-09-08 16:50:46 -0300

    semana 19

---
+++ main.py
@@ -0,0 +1,62 @@
+texto = 'Python' # tupla('P', 'y', 't', 'h', 'o', 'n')
+
+# imprimir letra da string com índice
+print(texto[3])
+
+# imprimir a última letra com índice
+print(texto[-1])
+
+# erro ao tentar atribuir um valor à tupla
+#texto[3] = 't'
+
+# é imutável, mas posso trocar o valor inteiro
+texto = 'Pythonia'
+print(texto)
+
+# .find - retorna o índice do texto desejado
+# obs.: retorna -1 caso não encontre o texto
+print(texto.find('n')) #5
+print(texto.find('f')) #-1
+
+# .index - retorna o índice do texto desejado
+# obs.: retorna ValueError caso não encontre o texto
+print(texto.index('n')) #5
+#print(texto.index('f')) #-1
+
+#  replace - substitui valor na string
+novo_texto = texto.replace('P', 'p')
+print(novo_texto)
+print(texto)
+
+# in
+#for letra in texto:
+#    print(letra)
+
+frase = 'gatos são melhores que cachorros'
+pesquisa = 'chor'
+if pesquisa in frase:
+    print(frase.index(pesquisa))
+else:
+    print(f'{pesquisa} não foi encontrado...')
+
+frase = 'a rápida raposa marrom pula sobre o cachorro preguiçoso'
+
+# slicing - retorna uma substring com índice do começo e fim
+subtexto = frase[9:15]
+print(frase)
+
+print(f'O texto {subtexto} está entre os índices 9 e 15')
+
+# texto = "BR-SP-2024-0042"
+# print(texto[-4:])
+
+valor = 1250.5
+print(f"R$ {valor:.2f}".replace(".", ","))
+print(f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+
+valor = 0.857
+print(f"{valor:.2%}".replace(".", ","))
+
+texto = "BR-ABC-SP-2024-0042"
+inicio = texto.find('SP')
+print(texto[inicio:inicio+7])
